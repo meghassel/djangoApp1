32ad5d7a7c2968b74a55ceef5851ce57eac0a593
MEGHASSEL Fethi <fet.meghassel@cnac.dz>
2023-04-25 12:13:02 +0100

    add stock application

--- djangoApp1/settings.py
+++ djangoApp1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stock',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,21 @@ WSGI_APPLICATION = 'djangoApp1.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'stock_manage',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 

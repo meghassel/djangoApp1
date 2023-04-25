2c620447a6b66ebd8d7acbc4019fb677092f08fe
MEGHASSEL Fethi <fet.meghassel@cnac.dz>
2023-04-25 14:41:13 +0100

    add gabarit template

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def productList (request) :
+    return HttpResponse("<h1>test view content</h1>")
+
+def homePage (request) :
+    return render(request,'home.html')

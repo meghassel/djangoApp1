f4836ddfb4e88e8ef6295672bde4e666fc43baad
MEGHASSEL Fethi <fet.meghassel@cnac.dz>
2023-04-25 12:21:53 +0100

    new product stock

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Product (models.Model):
+    name = models.fields.CharField(max_length=100)
+    category = models.fields.CharField(max_length=25)
+    price = models.fields.FloatField()
+    stock_qt = models.fields.IntegerField(max_length=5)
